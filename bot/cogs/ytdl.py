e37c5f5f3096a0499a228e466145906d110fc420
Geabot <xchgeaxeax@mailbox.org>
2025-04-06 16:07:24 -0400

    Add command to force schedule a download

--- bot/cogs/ytdl.py
+++ bot/cogs/ytdl.py
@@ -2,8 +2,24 @@ import logging
 import discord
 from discord.ext import commands, tasks
 from bot.downloader import downloader
+from datetime import datetime, timedelta, timezone
+import re
 import sys
 
+
+hammertime_regex = re.compile(r"<t:([0-9]+):?.*>")
+time_regex = re.compile(r'((?P<days>\d+?)d)?((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?')
+
+def parse_text_duration_timedelta(time_str):
+    parts = time_regex.match(time_str)
+    if not parts:
+        return None
+    parts = parts.groupdict()
+    time_params = {}
+    for name, param in parts.items():
+        if param:
+            time_params[name] = int(param)
+    return timedelta(**time_params)
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +28,17 @@ class YtDl(commands.Cog):
         self.bot = bot
         self.downloader = downloader.Downloader(self.bot)
         self.check_tasks.start()
+
+    def parse_text_as_datetime(self, time_text: str):
+        """Parses either a discord timestamp <t:unixepoch:F> or a human readable string 2d1h5m0s
+        as a datetime"""
+        if (matches := hammertime_regex.match(time_text)):
+            unix_epoch = int(matches.group(1))
+            return datetime.fromtimestamp(unix_epoch).astimezone(timezone.utc)
+        timedelta = parse_text_duration_timedelta(time_text)
+        if not timedelta:
+            return None
+        return datetime.now().astimezone(timezone.utc)+timedelta
 
     @commands.is_owner()
     @commands.hybrid_command(
@@ -37,6 +64,23 @@ class YtDl(commands.Cog):
                 self.downloader.defer_download_until_time(url, time, guild_id, channel_id)
                 formatted_dt = discord.utils.format_dt(time, style='F')
                 await ctx.send(f"Scheduling download for {formatted_dt}")
+
+
+    @commands.is_owner()
+    @commands.hybrid_command(
+        name="scheduled-download",
+        brief="Forces a video download at a specific time",
+        description="Forces a video download at a specific time",
+        usage="",
+    )
+    async def scheduled_download(self, ctx: commands.Context, url: str, timestamp:str):
+        channel_id = ctx.channel.id
+        guild_id = ctx.guild.id
+        time = self.parse_text_as_datetime(timestamp)
+        self.downloader.defer_download_until_time(url, time, guild_id, channel_id)
+        formatted_dt = discord.utils.format_dt(time, style='F')
+        await ctx.send(f"Scheduling download for {formatted_dt}")
+    
 
     @tasks.loop(minutes=1, reconnect=True)
     async def check_tasks(self):
